61cc2e376700975ac6625d60284ae418cbbd28e9
Vetriselvan Praveen <116857991+Praveen1e3r24@users.noreply.github.com>
2023-04-20 23:34:07 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -5,7 +5,7 @@ public class Question3
    public static void main(String[] args) {
   
     Scanner myobj = new Scanner(System.in);
-     System.out.println("Enter integer");
+     
        
     int integer = myobj.nextInt();
  
