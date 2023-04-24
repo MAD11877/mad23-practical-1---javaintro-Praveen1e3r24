37839040cf4cb231454f4057a084e6de1592b761
Vetriselvan Praveen <116857991+Praveen1e3r24@users.noreply.github.com>
2023-04-24 09:58:15 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -6,8 +6,8 @@ public class Question2
   public static void main(String[] args) {
   
     Scanner myobj = new Scanner(System.in);
-   double weight=myobj.nextDouble;
-   double height=myobj.nextDouble;
+   double weight=myobj.nextDouble();
+   double height=myobj.nextDouble();
 
     double bmi=weight/(height*height);
   System.out.println(bmi);
